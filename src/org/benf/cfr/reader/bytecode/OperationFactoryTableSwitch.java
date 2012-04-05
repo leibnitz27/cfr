1938d7a4a2e496bb0d8439d778c0052b48b2a2e0
Lee Benfield <lee@benf.org>
2012-04-05 05:09:44 +0000

    table switch test
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@5 c820dd76-da7a-4b58-9bdb-1b1215dfe356

--- src/org/benf/cfr/reader/bytecode/OperationFactoryTableSwitch.java
+++ src/org/benf/cfr/reader/bytecode/OperationFactoryTableSwitch.java
@@ -14,9 +14,30 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  */
 public class OperationFactoryTableSwitch extends OperationFactoryDefault {
 
+    // offsets relative to computed start of default
+    private static final int OFFSET_OF_DEFAULT = 0;
+    private static final int OFFSET_OF_LOWBYTE = 4;
+    private static final int OFFSET_OF_HIGHBYTE = 8;
+    private static final int OFFSET_OF_OFFSETS = 12;
+
+
     @Override
-    public Op01WithProcessedDataAndByteJumps createOperation(JVMInstr instr, ByteData bd, ConstantPool cp, int offset)
-    {
+    public Op01WithProcessedDataAndByteJumps createOperation(JVMInstr instr, ByteData bd, ConstantPool cp, int offset) {
+        int curoffset = offset + 1;
+        // We need to align the next byte to a 4 byte boundary relative to the start of the method.
+        int overflow = (curoffset % 4);
+        overflow = overflow > 0 ? 4 - overflow : 0;
+        int startdata = 1 + overflow;
+        int defaultvalue = bd.getU4At(startdata + OFFSET_OF_DEFAULT);
+        int lowvalue = bd.getU4At(startdata + OFFSET_OF_LOWBYTE);
+        int highvalue = bd.getU4At(startdata + OFFSET_OF_HIGHBYTE);
+        int numoffsets = highvalue - lowvalue + 1;
+        for (int x = 0; x < numoffsets; ++x) {
+            int jumpfor = bd.getU4At(startdata + OFFSET_OF_OFFSETS + 4 * x);
+            System.out.println("Jump for " + (lowvalue + x) + " = " + jumpfor);
+        }
+
+
         throw new NotImplementedException();
     }
 
---
+++ src/org/benf/cfr/tests/SwitchTest1.java
@@ -0,0 +1,53 @@
+package org.benf.cfr.tests;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lee
+ * Date: 05/05/2011
+ * Time: 18:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class SwitchTest1 {
+
+    enum enm {
+        ONE,
+        TWO
+    }
+
+    ;
+
+
+    // Tableswitch
+    public void test1(int x) {
+        switch (x) {
+            case 1:
+                System.out.println("One");   // Fall through
+            case 3:
+                System.out.println("Three");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 5:
+                System.out.println("Five"); // Fall through
+            default:
+                System.out.println("Default");
+
+        }
+    }
+    /*
+    public int test0(enm e) {
+        switch (e) {
+            case ONE:
+                return 1;
+            case TWO:
+                return 2;
+        }
+        return 0;
+    }
+    */
+}
