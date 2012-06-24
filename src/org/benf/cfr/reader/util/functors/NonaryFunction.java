a224354511f0d3a2dc802d5a76340d07f5d96f27
Lee Benfield <lee@benf.org>
2012-06-24 20:35:30 +0000

    Nonary :)
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@64 c820dd76-da7a-4b58-9bdb-1b1215dfe356

---
+++ src/org/benf/cfr/reader/util/functors/NonaryFunction.java
@@ -0,0 +1,11 @@
+package org.benf.cfr.reader.util.functors;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lee
+ * Date: 23/06/2012
+ * Time: 10:33
+ */
+public interface NonaryFunction<T> {
+    T invoke();
+}
