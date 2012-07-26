2262a615b93ec3914fc308a686f82bf9297b3694
Lee Benfield <lee@benf.org>
2012-07-26 16:25:08 +0000

    remove .<init> from constructors
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@121 c820dd76-da7a-4b58-9bdb-1b1215dfe356

--- src/org/benf/cfr/reader/bytecode/analysis/parse/expression/ConstructorInvokation.java
+++ src/org/benf/cfr/reader/bytecode/analysis/parse/expression/ConstructorInvokation.java
@@ -18,7 +18,6 @@ import java.util.List;
  * User: lee
  * Date: 16/03/2012
  * Time: 17:26
- * To change this template use File | Settings | File Templates.
  */
 public class ConstructorInvokation extends AbstractExpression {
     private final ConstantPoolEntryMethodRef function;
@@ -45,11 +44,7 @@ public class ConstructorInvokation extends AbstractExpression {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("new " + cp.getUTF8Entry(type.getNameIndex()).getValue());
-        // + ".");
-        // ConstantPoolEntryNameAndType nameAndType = cp.getNameAndTypeEntry(function.getNameAndTypeIndex());
-        //sb.append(nameAndType.getName(cp).getValue());
-        sb.append("(");
+        sb.append("new ").append(cp.getUTF8Entry(type.getNameIndex()).getValue()).append("(");
         boolean first = true;
         for (Expression arg : args) {
             if (!first) sb.append(", ");
