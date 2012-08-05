b1be8745d6a9947c05ac9a255f35dacfb55fe9b8
Lee Benfield <lee@benf.org>
2012-08-05 16:27:35 +0000

    Another test
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@131 c820dd76-da7a-4b58-9bdb-1b1215dfe356

---
+++ src/org/benf/cfr/tests/ArrayTest3.java
@@ -0,0 +1,22 @@
+package org.benf.cfr.tests;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lee
+ * Date: 05/05/2011
+ * Time: 06:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArrayTest3 {
+    int x;
+    int y;
+    int z;
+
+    void test1(int a, int b) {
+        String[] tmp = new String[]{"a", "b", "c"};
+        for (String s : tmp) {
+            System.out.println(s);
+        }
+    }
+
+}
