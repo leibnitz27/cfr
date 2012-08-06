a048f482dfc3dc9d78c88ce05f0f35ab72d58d18
Lee Benfield <lee@benf.org>
2012-08-06 15:57:08 +0000

    Test showing switch on string.
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@135 c820dd76-da7a-4b58-9bdb-1b1215dfe356

---
+++ src/org/benf/cfr/tests/SwitchTest4.java
@@ -0,0 +1,32 @@
+package org.benf.cfr.tests;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lee
+ * Date: 05/05/2011
+ * Time: 18:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class SwitchTest4 {
+
+
+    public int test0(String s) {
+        switch (s) {
+            default:
+                System.out.println("Test");
+                break;
+            case "FRED":
+                return 1;
+            case "JIM":
+                return 2;
+            case "BB":  // BB and Aa have the same hashcode.
+                return 12;
+            case "Aa":
+                return 13;
+        }
+        System.out.println("Here");
+        return 0;
+    }
+
+
+}
