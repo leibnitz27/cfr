7d9d72bc20da80adf3847466f8fdfbb2a8341267
Lee Benfield <lee@benf.org>
2012-09-20 05:27:59 +0000

    more tests
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@196 c820dd76-da7a-4b58-9bdb-1b1215dfe356

--- src/org/benf/cfr/tests/LoopTest10.java
+++ src/org/benf/cfr/tests/LoopTest10.java
@@ -20,8 +20,15 @@ public class LoopTest10 {
 
     public void test2(int end) {
         int c = 0;
-        while (c++ < end || ++c < end) {
-            System.out.println(":" + c);
+        while (++c < end || c++ < end) {
+            System.out.println(":" + ++c);
+        }
+    }
+
+    public void test3(int end) {
+        int c = 0;
+        while (c++ < end || c++ < end) {
+            System.out.println(":" + c++);
         }
     }
 }
