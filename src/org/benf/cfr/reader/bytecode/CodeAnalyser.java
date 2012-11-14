ab07227c864294743285f930e369025ad772e208
Lee Benfield <lee@benf.org>
2012-11-14 07:16:18 +0000

    remove debug
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@205 c820dd76-da7a-4b58-9bdb-1b1215dfe356

--- src/org/benf/cfr/reader/bytecode/CodeAnalyser.java
+++ src/org/benf/cfr/reader/bytecode/CodeAnalyser.java
@@ -143,8 +143,8 @@ public class CodeAnalyser {
         // Expand any 'multiple' statements (eg from dups)
         Op03SimpleStatement.flattenCompoundStatements(op03SimpleParseNodes);
 
-        dumper.print("Raw Op3 statements:\n");
-        op03SimpleParseNodes.get(0).dump(dumper);
+//        dumper.print("Raw Op3 statements:\n");
+//        op03SimpleParseNodes.get(0).dump(dumper);
 
 //        Op03SimpleStatement.findGenericTypes(op03SimpleParseNodes, cp);
 
@@ -163,8 +163,8 @@ public class CodeAnalyser {
         Op03SimpleStatement.condenseLValues(op03SimpleParseNodes);
         op03SimpleParseNodes = Op03SimpleStatement.renumber(op03SimpleParseNodes);
 
-        dumper.print("Raw Op3 statements:\n");
-        op03SimpleParseNodes.get(0).dump(dumper);
+//        dumper.print("Raw Op3 statements:\n");
+//        op03SimpleParseNodes.get(0).dump(dumper);
 
 
         // Try to eliminate catch temporaries.
