c8d1cfb209ff2457d496cc41646aa908c0ae5bc5
Lee Benfield <lee@benf.org>
2013-03-06 17:58:01 +0000

    Further generifying
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@287 c820dd76-da7a-4b58-9bdb-1b1215dfe356

--- src/org/benf/cfr/reader/bytecode/analysis/parse/utils/LValueAssignmentScopeDiscoverer.java
+++ src/org/benf/cfr/reader/bytecode/analysis/parse/utils/LValueAssignmentScopeDiscoverer.java
@@ -5,6 +5,7 @@ import org.benf.cfr.reader.bytecode.analysis.parse.LValue;
 import org.benf.cfr.reader.bytecode.analysis.parse.StatementContainer;
 import org.benf.cfr.reader.bytecode.analysis.parse.lvalue.LocalVariable;
 import org.benf.cfr.reader.bytecode.analysis.parse.lvalue.StackSSALabel;
+import org.benf.cfr.reader.bytecode.analysis.structured.StructuredStatement;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,24 +13,24 @@ import org.benf.cfr.reader.bytecode.analysis.parse.lvalue.StackSSALabel;
  * Date: 06/03/2013
  * Time: 06:09
  */
-public class LValueAssignmentScopeDiscoverer implements LValueAssignmentCollector {
+public class LValueAssignmentScopeDiscoverer implements LValueAssignmentCollector<StructuredStatement> {
     @Override
-    public void collect(StackSSALabel lValue, StatementContainer statementContainer, Expression value) {
+    public void collect(StackSSALabel lValue, StatementContainer<StructuredStatement> statementContainer, Expression value) {
 
     }
 
     @Override
-    public void collectMultiUse(StackSSALabel lValue, StatementContainer statementContainer, Expression value) {
+    public void collectMultiUse(StackSSALabel lValue, StatementContainer<StructuredStatement> statementContainer, Expression value) {
 
     }
 
     @Override
-    public void collectMutatedLValue(LValue lValue, StatementContainer statementContainer, Expression value) {
+    public void collectMutatedLValue(LValue lValue, StatementContainer<StructuredStatement> statementContainer, Expression value) {
 
     }
 
     @Override
-    public void collectLocalVariableAssignment(LocalVariable localVariable, StatementContainer statementContainer, Expression value) {
+    public void collectLocalVariableAssignment(LocalVariable localVariable, StatementContainer<StructuredStatement> statementContainer, Expression value) {
 
     }
 }
