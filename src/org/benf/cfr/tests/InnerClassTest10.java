7d899beeb6d081fa37743b5b69fb075c84d1ccf4
Lee Benfield <lee@benf.org>
2013-04-05 05:48:03 +0000

    0_10
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@352 c820dd76-da7a-4b58-9bdb-1b1215dfe356

--- src/org/benf/cfr/tests/InnerClassTest10.java
+++ src/org/benf/cfr/tests/InnerClassTest10.java
@@ -12,7 +12,7 @@ public class InnerClassTest10 {
 
     public class Inner1 {
 
-        public InnerClassTest4.InnerBase tweakX(int y) {
+        public InnerClassTest4.InnerBase tweak(int y) {
             return new InnerClassTest4.InnerBase(SetFactory.newSet(y));
         }
 
