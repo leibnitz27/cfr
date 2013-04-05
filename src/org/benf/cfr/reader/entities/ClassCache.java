1a2e204761c029f469353fe2134dfebf65d0fc12
Lee Benfield <lee@benf.org>
2013-04-05 05:30:10 +0000

    Improve display of inner class names.
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@350 c820dd76-da7a-4b58-9bdb-1b1215dfe356

--- src/org/benf/cfr/reader/entities/ClassCache.java
+++ src/org/benf/cfr/reader/entities/ClassCache.java
@@ -25,24 +25,54 @@ public class ClassCache {
     private final Map<String, JavaRefTypeInstance> refClassTypeCache = MapFactory.newMap();
     private final Set<String> importableClasses = SetFactory.newSet();
 
+    private transient JavaRefTypeInstance analysisType;
+
+    private boolean importClass(JavaRefTypeInstance clazz) {
+        InnerClassInfo innerClassInfo = clazz.getInnerClassHereInfo();
+        if (!innerClassInfo.isInnerClass()) return true;
+        if (clazz.getRawName().startsWith(analysisType.getRawName())) return false;
+        return true;
+    }
+
+    private String generateInnerClassShortName(JavaRefTypeInstance clazz) {
+        InnerClassInfo innerClassInfo = clazz.getInnerClassHereInfo();
+        String clazzRawName = clazz.getRawName();
+        /* Local inner class.  We want the smallest postfix
+         */
+        if (clazz.getRawName().startsWith(analysisType.getRawName())) {
+            JavaRefTypeInstance parent = innerClassInfo.getOuterClass();
+            if (parent == null) return clazzRawName;
+            return clazzRawName.substring(parent.getRawName().length() + 1);
+        }
+        /*
+         * Foreign inner class.  Take the existing determined shortname, and replace $ with '.'.
+         */
+
+        return longNameToShortName.get(clazzRawName).replace('$', '.');
+    }
+
     private void markClassNameUsed(JavaRefTypeInstance typeInstance) {
+        /*
+         * While it's a bit of a hack, the first type which is marked as used is always the type under
+         * analysis.
+         */
+        if (analysisType == null) analysisType = typeInstance;
+
         String className = typeInstance.getRawName();
         int idxlast = className.lastIndexOf('.');
         String partname = idxlast == -1 ? className : className.substring(idxlast + 1);
-        InnerClassInfo innerClassInfo = typeInstance.getInnerClassHereInfo();
         if (!shortNameToLongName.containsKey(partname)) {
             shortNameToLongName.put(partname, className);
             longNameToShortName.put(className, partname);
-            if (!innerClassInfo.isInnerClass()) { // actually, should only veto inners of main class.
+            if (importClass(typeInstance)) {
                 importableClasses.add(className);
             }
         }
-        if (innerClassInfo.isInnerClass()) {
-            JavaRefTypeInstance outerClass = innerClassInfo.getOuterClass();
-            if (outerClass != null) {
-                String innerName = className.substring(outerClass.getRawName().length() + 1);
-                longNameToShortName.put(className, innerName);
-            }
+        /*
+         * Override longname to short name for inner classes.
+         */
+        if (typeInstance.getInnerClassHereInfo().isInnerClass()) {
+            longNameToShortName.put(className, generateInnerClassShortName(typeInstance));
         }
     }
 
