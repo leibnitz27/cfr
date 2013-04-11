317d118ba76f3aa07822b4393802740dd8a015e1
Lee Benfield <lee@benf.org>
2013-04-11 16:53:22 +0000

    Anonymous inner class test
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@354 c820dd76-da7a-4b58-9bdb-1b1215dfe356

---
+++ src/org/benf/cfr/tests/AnonymousInnerClassTest1.java
@@ -0,0 +1,34 @@
+package org.benf.cfr.tests;
+
+import org.benf.cfr.reader.util.functors.UnaryFunction;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lee
+ * Date: 11/04/2013
+ * Time: 17:49
+ */
+public class AnonymousInnerClassTest1 {
+
+    Integer invoker(int arg, UnaryFunction<Integer, Integer> fn) {
+        return fn.invoke(arg);
+    }
+
+    public int doit(int x) {
+        return invoker(x, new UnaryFunction<Integer, Integer>() {
+            @Override
+            public Integer invoke(Integer arg) {
+                return arg * 3;
+            }
+        });
+    }
+
+    public int doit2(int x) {
+        return invoker(x, new UnaryFunction<Integer, Integer>() {
+            @Override
+            public Integer invoke(Integer arg) {
+                return arg * 3;
+            }
+        });
+    }
+}
