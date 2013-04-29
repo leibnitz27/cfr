8623ad50d964c4185972dc2d439b16297dddd73b
Lee Benfield <lee@benf.org>
2013-04-29 04:48:20 +0000

    Slight improvement to anonymous inners.
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@402 c820dd76-da7a-4b58-9bdb-1b1215dfe356

---
+++ src/org/benf/cfr/reader/util/output/CommaHelp.java
@@ -0,0 +1,19 @@
+package org.benf.cfr.reader.util.output;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lee
+ * Date: 23/04/2013
+ * Time: 06:49
+ */
+public class CommaHelp {
+    public static boolean comma(boolean first, StringBuilder sb) {
+        if (!first) sb.append(", ");
+        return false;
+    }
+
+    public static boolean comma(boolean first, Dumper d) {
+        if (!first) d.print(", ");
+        return false;
+    }
+}
