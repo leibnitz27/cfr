eb0c8e7c487f4bce594f1787068b5ff2e97b28a1
Lee Benfield <lee@benf.org>
2013-05-07 16:27:14 +0000

    enum with member and statics
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@450 c820dd76-da7a-4b58-9bdb-1b1215dfe356

---
+++ src/org/benf/cfr/tests/EnumTest2.java
@@ -0,0 +1,24 @@
+package org.benf.cfr.tests;
+
+import org.benf.cfr.reader.util.MapFactory;
+
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lee
+ * Date: 22/08/2012
+ * Time: 20:58
+ */
+public enum EnumTest2 {
+    FOO(2),
+    BAR(1),
+    BAP(5);
+
+    private final int x;
+    private final static Map<String, EnumTest2> smap = MapFactory.newMap();
+
+    private EnumTest2(int x) {
+        this.x = x;
+    }
+}
