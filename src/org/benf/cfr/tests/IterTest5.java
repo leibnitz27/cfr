ce986dd03af28271abbd7347d494ce97beac56ba
Lee Benfield <lee@benf.org>
2013-05-10 05:05:02 +0000

    More tests
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@461 c820dd76-da7a-4b58-9bdb-1b1215dfe356

--- src/org/benf/cfr/tests/IterTest5.java
+++ src/org/benf/cfr/tests/IterTest5.java
@@ -16,7 +16,7 @@ public class IterTest5 {
     public boolean test1(List<String> list, Set<Integer> set) {
         boolean result = false;
         int x = 3;
-        int y = 1;
+        int y = 1; // you can't tell this is an int.
         for (x = 1; x < 12; ++x) {    // use of an itervar already in scope.
             System.out.println(x);
         }
