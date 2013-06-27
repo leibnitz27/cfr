db75745da2b1a4a2e8787e066831734a0308a20d
Lee Benfield <lee@benf.org>
2013-06-27 11:01:03 +0000

    0_16
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@536 c820dd76-da7a-4b58-9bdb-1b1215dfe356

--- src/org/benf/cfr/reader/util/MiscConstants.java
+++ src/org/benf/cfr/reader/util/MiscConstants.java
@@ -7,7 +7,8 @@ package org.benf.cfr.reader.util;
  * Time: 05:44
  */
 public interface MiscConstants {
-    public static final String CFR_HEADER = "/*\n * Decompiled with CFR.\n */\n";
+    public static final String CFR_VERSION = "0_16";
+    public static final String CFR_HEADER = "/*\n * Decompiled with CFR " + CFR_VERSION + ".\n */\n";
 
     public static final String INIT_METHOD = "<init>";
     public static final String STATIC_INIT_METHOD = "<clinit>";
