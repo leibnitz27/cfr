7f6d532f63130fba1a2f836eef19e11399b188ee
Lee Benfield <lee@benf.org>
2013-12-05 21:54:32 +0000

    0_55 (again)
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@806 c820dd76-da7a-4b58-9bdb-1b1215dfe356

--- src/org/benf/cfr/reader/util/MiscConstants.java
+++ src/org/benf/cfr/reader/util/MiscConstants.java
@@ -4,7 +4,6 @@ package org.benf.cfr.reader.util;
  * Created with IntelliJ IDEA.
  * User: lee
  * Date: 01/05/2013
- * Time: 05:44
  */
 public interface MiscConstants {
     public static final String CFR_VERSION = "0_55";
