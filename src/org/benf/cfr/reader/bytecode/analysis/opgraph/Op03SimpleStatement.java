8044064e766228404e9387963655919a66f2f975
Lee Benfield <lee@benf.org>
2014-01-14 18:01:05 +0000

    Improve synchronised block handling (slightly!)
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@848 c820dd76-da7a-4b58-9bdb-1b1215dfe356

--- src/org/benf/cfr/reader/bytecode/analysis/opgraph/Op03SimpleStatement.java
+++ src/org/benf/cfr/reader/bytecode/analysis/opgraph/Op03SimpleStatement.java
@@ -5353,7 +5353,24 @@ public class Op03SimpleStatement implements MutableGraph<Op03SimpleStatement>, D
     }
 
 
-    public static void findSynchronizedStart(final Op03SimpleStatement start, final Expression monitor) {
+    /*
+     * Strictly speaking, this isn't true.....  We should verify elsewhere first that we don't push
+     * operations through a monitorexit, as that will change the semantics.
+     */
+    private static boolean anyOpHasEffect(List<Op03SimpleStatement> ops) {
+        for (Op03SimpleStatement op : ops) {
+            Statement stm = op.getStatement();
+            Class<?> stmcls = stm.getClass();
+            if (stmcls == GotoStatement.class) continue;
+            if (stmcls == ThrowStatement.class) continue;
+            if (stmcls == CommentStatement.class) continue;
+            if (stm instanceof ReturnStatement) continue;
+            return true;
+        }
+        return false;
+    }
+
+    public static void findSynchronizedRange(final Op03SimpleStatement start, final Expression monitor) {
         final Set<Op03SimpleStatement> addToBlock = SetFactory.newSet();
 
         final Set<Op03SimpleStatement> foundExits = SetFactory.newSet();
@@ -5404,6 +5421,7 @@ public class Op03SimpleStatement implements MutableGraph<Op03SimpleStatement>, D
                         if (statement instanceof MonitorExitStatement) {
                             if (monitor.equals(((MonitorExitStatement) statement).getMonitor())) {
                                 foundExits.add(arg1);
+                                addToBlock.add(arg1);
                                 /*
                                  * If there's a return / throw / goto immediately after this, then we know that the brace
                                  * is validly moved.
@@ -5438,16 +5456,68 @@ public class Op03SimpleStatement implements MutableGraph<Op03SimpleStatement>, D
         );
         marker.process();
 
+        /*
+         * An extra pass, wherein we find all blocks which members of addtoblock are in.
+         * (and the initial start is not in.)
+         * This is because we need to handle synchronized blocks which may not fit into the natural
+         * ordering.
+         *
+         * monitorenter
+         * bip
+         * if
+         *   foo
+         *   monitorexit
+         *   bar
+         * else
+         *   bop
+         *   monitorexit
+         *   bam
+         */
+        addToBlock.remove(start);
+        /*
+         * find entries with same-block targets which are NOT in addToBlock, add them.
+         */
+        Set<Op03SimpleStatement> requiredComments = SetFactory.newSet();
+        Iterator<Op03SimpleStatement> foundExitIter = foundExits.iterator();
+        while (foundExitIter.hasNext()) {
+            final Op03SimpleStatement foundExit = foundExitIter.next();
+            final Set<BlockIdentifier> exitBlocks = SetFactory.newSet(foundExit.getBlockIdentifiers());
+            exitBlocks.removeAll(start.getBlockIdentifiers());
+            final List<Op03SimpleStatement> added = ListFactory.newList();
+            GraphVisitor<Op03SimpleStatement> additional = new GraphVisitorDFS<Op03SimpleStatement>(foundExit, new BinaryProcedure<Op03SimpleStatement, GraphVisitor<Op03SimpleStatement>>() {
+                @Override
+                public void call(Op03SimpleStatement arg1, GraphVisitor<Op03SimpleStatement> arg2) {
+                    if (SetUtil.hasIntersection(exitBlocks, arg1.getBlockIdentifiers())) {
+                        if (arg1 == foundExit) {
+                            arg2.enqueue(arg1.getTargets());
+                        } else if (addToBlock.add(arg1)) {
+                            added.add(arg1);
+                            arg2.enqueue(arg1.getTargets());
+                        }
+                    }
+                }
+            });
+            additional.process();
+            // If we had an effect, then we want to redesignate this monitor exit as a 'required comment'
+            if (anyOpHasEffect(added)) {
+                requiredComments.add(foundExit);
+                foundExitIter.remove();
+            }
+        }
+
+
         MonitorEnterStatement monitorEnterStatement = (MonitorEnterStatement) (start.containedStatement);
         BlockIdentifier blockIdentifier = monitorEnterStatement.getBlockIdentifier();
         for (Op03SimpleStatement contained : addToBlock) {
-            if (contained != start) {
-                contained.containedInBlocks.add(blockIdentifier);
-            }
+            contained.containedInBlocks.add(blockIdentifier);
         }
 
         for (Op03SimpleStatement exit : foundExits) {
             exit.nopOut();
+        }
+
+        for (Op03SimpleStatement exit : requiredComments) {
+            exit.replaceStatement(new CommentStatement("MONITOREXIT " + exit));
         }
 
         /* For the extra nodes, if ALL the sources are in the block, we add the extranode
@@ -5485,15 +5555,15 @@ public class Op03SimpleStatement implements MutableGraph<Op03SimpleStatement>, D
     * What would be nasty is a switch statement which enters on one branch and exits on another...
     */
     public static void findSynchronizedBlocks(List<Op03SimpleStatement> statements) {
-        List<Op03SimpleStatement> exits = Functional.filter(statements, new TypeFilter<MonitorEnterStatement>(MonitorEnterStatement.class));
+        List<Op03SimpleStatement> enters = Functional.filter(statements, new TypeFilter<MonitorEnterStatement>(MonitorEnterStatement.class));
         // Each exit can be tied to one enter, which is the first one found by
         // walking code backwards and not passing any other exit/enter for this var.
         // (Every exit from a synchronised block has to exit, so if there's any possibiliy of an exception... )
 
-        for (Op03SimpleStatement exit : exits) {
-            MonitorEnterStatement monitorExitStatement = (MonitorEnterStatement) exit.containedStatement;
+        for (Op03SimpleStatement enter : enters) {
+            MonitorEnterStatement monitorExitStatement = (MonitorEnterStatement) enter.containedStatement;
 
-            findSynchronizedStart(exit, monitorExitStatement.getMonitor());
+            findSynchronizedRange(enter, monitorExitStatement.getMonitor());
         }
     }
 
