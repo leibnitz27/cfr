a8488cf8f6e3e9a96247c329d227a4e716042f39
Lee Benfield <lee@benf.org>
2014-03-21 06:38:07 +0000

    Dump foreign references to switches
    git-svn-id: file:///Users/lee/SVNrep/decompiler/trunk@938 c820dd76-da7a-4b58-9bdb-1b1215dfe356

--- src/org/benf/cfr/reader/bytecode/analysis/structured/statement/StructuredSwitch.java
+++ src/org/benf/cfr/reader/bytecode/analysis/structured/statement/StructuredSwitch.java
@@ -43,6 +43,7 @@ public class StructuredSwitch extends AbstractStructuredBlockStatement implement
 
     @Override
     public Dumper dump(Dumper dumper) {
+        if (blockIdentifier.hasForeignReferences()) dumper.print(blockIdentifier.getName() + " : ");
         dumper.print("switch (").dump(switchOn).print(") ");
         getBody().dump(dumper);
         return dumper;
