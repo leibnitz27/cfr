18c5323a0d8649dd1763ac288e4c92a2d4009f8d
Lee <lee@benf.org>
2014-07-06 18:13:03 +0100

    Better error when dealing with utterly nonsensical loop emission

--- src/org/benf/cfr/reader/bytecode/analysis/opgraph/op3rewriters/LoopIdentifier.java
+++ src/org/benf/cfr/reader/bytecode/analysis/opgraph/op3rewriters/LoopIdentifier.java
@@ -364,6 +364,10 @@ public class LoopIdentifier {
                 lastPostBlock = currentIdx;
             } while (true);
             if (lastPostBlock != postBlockIdx) {
+                // We will have rewritten a COMPLETELY impossible loop - but we're already too far gone.
+                if (!lastJump.getTargets().contains(start)) {
+                    throw new ConfusedCFRException("Nonsensican loop would be emitted - failure");
+                }
                 final Op03SimpleStatement afterNewJump = statements.get(lastPostBlock);
                 // Find after statement.  Insert a jump forward (out) here, and then insert
                 // a synthetic lastJump.  The previous lastJump should now jump to our synthetic
