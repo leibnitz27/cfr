73c06abf76d682be29462af6a4e76d1cf8f3d7f1
Lee <lee@benf.org>
2014-09-05 17:47:47 +0100

    Add iterable placeholder

--- src/org/benf/cfr/reader/bytecode/analysis/types/TypeConstants.java
+++ src/org/benf/cfr/reader/bytecode/analysis/types/TypeConstants.java
@@ -5,6 +5,7 @@ public interface TypeConstants {
     public final static JavaRefTypeInstance ENUM = JavaRefTypeInstance.createTypeConstant("java.lang.Enum", "Enum", OBJECT);
     public final static JavaRefTypeInstance ASSERTION_ERROR = JavaRefTypeInstance.createTypeConstant("java.lang.AssertionError", "AssertionError", OBJECT);
     public final static JavaRefTypeInstance STRING = JavaRefTypeInstance.createTypeConstant("java.lang.String", "String", OBJECT);
+    public final static JavaRefTypeInstance ITERABLE = JavaRefTypeInstance.createTypeConstant("java.lang.Iterable", "Iterable", OBJECT);
 
     public final static String boxingNameBoolean = "java.lang.Boolean";
     public final static String boxingNameByte = "java.lang.Byte";
