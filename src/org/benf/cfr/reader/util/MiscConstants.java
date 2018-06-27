ecc39415fd7eaffbd8024700388d8f714c6fa13e
Lee <lee@benf.org>
2018-06-27 06:56:29 +0100

    0_131

--- src/org/benf/cfr/reader/util/MiscConstants.java
+++ src/org/benf/cfr/reader/util/MiscConstants.java
@@ -1,19 +1,19 @@
 package org.benf.cfr.reader.util;
 
 public interface MiscConstants {
-    public static final String CFR_VERSION = "0_130";
-    
-    public static final String CFR_HEADER_BRA = "Decompiled with CFR";
+    String CFR_VERSION = "0_131";
 
-    public static final String INIT_METHOD = "<init>";
-    public static final String STATIC_INIT_METHOD = "<clinit>";
-    public static final String THIS = "this";
-    public static final String UNBOUND_GENERIC = "?";
-    public static final char INNER_CLASS_SEP_CHAR = '$';
-    public static final String INNER_CLASS_SEP_STR = "$";
+    String CFR_HEADER_BRA = "Decompiled with CFR";
 
-    public static final String DESERIALISE_LAMBDA_METHOD = "$deserializeLambda$";
-    public static final String SCALA_SERIAL_VERSION = "serialVersionUID";
-    public static final String GET_CLASS_NAME = "getClass";
-    public static final String REQUIRE_NON_NULL = "requireNonNull";
+    String INIT_METHOD = "<init>";
+    String STATIC_INIT_METHOD = "<clinit>";
+    String THIS = "this";
+    String UNBOUND_GENERIC = "?";
+    char INNER_CLASS_SEP_CHAR = '$';
+    String INNER_CLASS_SEP_STR = "$";
+
+    String DESERIALISE_LAMBDA_METHOD = "$deserializeLambda$";
+    String SCALA_SERIAL_VERSION = "serialVersionUID";
+    String GET_CLASS_NAME = "getClass";
+    String REQUIRE_NON_NULL = "requireNonNull";
 }
