9f8a18676889e581e7f323559fbca765ae6a0c23
Lee <lee@benf.org>
2019-08-12 18:22:08 +0100

    Allow try resources to be collapsed up into non resource tries.

--- src/org/benf/cfr/reader/bytecode/analysis/opgraph/op4rewriters/transformers/TryResourcesCollapser.java
+++ src/org/benf/cfr/reader/bytecode/analysis/opgraph/op4rewriters/transformers/TryResourcesCollapser.java
@@ -13,8 +13,7 @@ public class TryResourcesCollapser implements StructuredStatementTransformer {
         root.transform(this, structuredScope);
     }
 
-    private StructuredTry transform(StructuredTry stm) {
-        if (!stm.hasResources()) return stm;
+    private StructuredTry combineTryResources(StructuredTry stm) {
         Op04StructuredStatement content = stm.getInline();
         StructuredStatement inside = content.getStatement();
         if (inside instanceof Block) {
@@ -29,13 +28,15 @@ public class TryResourcesCollapser implements StructuredStatementTransformer {
         if (!inner.getCatchBlocks().isEmpty()) return stm;
         stm.addResources(inner.getResources());
         stm.setTryBlock(inner.getInline());
-        return transform(stm);
+        // We recurse here - that's ok, because if we have to do work, we won't
+        // *REDO* it, as we'll have already combined.
+        return combineTryResources(stm);
     }
 
     @Override
     public StructuredStatement transform(StructuredStatement in, StructuredScope scope) {
         if (in instanceof StructuredTry) {
-            in = transform((StructuredTry)in);
+            in = combineTryResources((StructuredTry)in);
         }
         in.transformStructuredChildren(this, scope);
         return in;
