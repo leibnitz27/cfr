3b55b367abd0995dcf58d4457c34f809a384a231
Hackubau <marco.guassone@gmail.com>
2020-12-22 17:14:51 +0100

    Update cfr-loop.sh
    shell updated

--- cfr-loop.sh
+++ cfr-loop.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
+#developed by marco guassone 2020/12/22
 
 #CFR .jar file
 BINARY=$1
 
+#EXTENSION TO ANALYZE
+EXT=$2
+
 #FOLDER TO DECOMPILE
-FOLDER=$2
+FOLDER=$3
 
 #OUTPUT WHERE DECOMPILE
-OUTPUT=$3
+OUTPUT=$4
 
-if [[ $# -lt 3 ]] ; then
-    echo 'Please provide: <path to cfr jar> <path to folder to analyze> <output folder>'
+
+if [[ $# -lt 4 ]] ; then
+
+    echo ''
+    echo ''
+    echo 'Please provide the following parameters: '
+    echo '<path to cfr jar> <extension to analyze> <path to folder to analyze> <output folder>'
+    echo ''
+    echo ''
+    echo 'EXAMPLE:'
+    echo ''
+    echo ''
+    echo './cfr-loop.sh ./cfr-0.150.jar .class /path/to/my/project-root /tmp/a-new-or-existing-folder-as-you-want'
     exit 1
 fi
 
-for f in $FOLDER/* $FOLDER/**/* ; do
-    if [[ -d $f ]]; then
-        echo "$f is a directory"
-    elif [[ -f $f ]]; then
-        java -jar $BINARY $f --outputdir $OUTPUT
-    fi
-done;
+set -f
+for f in $(find "$FOLDER" -name *.class)
+do
+  java -jar $BINARY $f --outputdir $OUTPUT
+done
