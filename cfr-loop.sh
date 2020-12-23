04effa0f66b246b55f83df25a80651e1248cb74a
Hackubau <marco.guassone@gmail.com>
2020-12-23 10:04:45 +0100

    Update cfr-loop.sh
    fixed ext variable and added SORT option (some class are decompiled differently by others and some time it's just a matter of ordering, so, if you need a second check, you can brutally alphabetically sort the entire java file to hope your compare can check if the classes are the same)

--- cfr-loop.sh
+++ cfr-loop.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#developed by marco guassone 2020/12/22
 
 #CFR .jar file
 BINARY=$1
@@ -13,24 +12,34 @@ FOLDER=$3
 #OUTPUT WHERE DECOMPILE
 OUTPUT=$4
 
+#SORT FILE CONTENT
+SORT=$5
+
 
 if [[ $# -lt 4 ]] ; then
-
-    echo ''
-    echo ''
     echo 'Please provide the following parameters: '
     echo '<path to cfr jar> <extension to analyze> <path to folder to analyze> <output folder>'
     echo ''
-    echo ''
     echo 'EXAMPLE:'
-    echo ''
     echo ''
     echo './cfr-loop.sh ./cfr-0.150.jar .class /path/to/my/project-root /tmp/a-new-or-existing-folder-as-you-want'
     exit 1
 fi
 
+echo "decompiling ..."
 set -f
-for f in $(find "$FOLDER" -name *.class)
+for f in $(find "$FOLDER" -name "*$EXT")
 do
   java -jar $BINARY $f --outputdir $OUTPUT
 done
+echo "decompiling done"
+
+if [[ $SORT -eq sort ]] ; then
+  echo "sorting ..."
+  set -f
+  for fjava in $(find "$OUTPUT" -name *.java)
+  do
+    sort $fjava --output=$fjava
+  done
+  echo "sorting done"
+fi
