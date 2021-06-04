903e108c2fbe7a188f4785407a4e9b155d8a1f51
Lee <lee@benf.org>
2021-06-04 22:58:28 +0100

    Tighten record support post j15 changes.

--- src/org/benf/cfr/reader/bytecode/analysis/parse/rewriters/AbstractExpressionRewriter.java
+++ src/org/benf/cfr/reader/bytecode/analysis/parse/rewriters/AbstractExpressionRewriter.java
@@ -21,7 +21,7 @@ public abstract class AbstractExpressionRewriter implements ExpressionRewriter {
 
     @Override
     public LValue rewriteExpression(LValue lValue, SSAIdentifiers ssaIdentifiers, StatementContainer statementContainer, ExpressionRewriterFlags flags) {
-        return lValue;
+        return lValue.applyExpressionRewriter(this, ssaIdentifiers, statementContainer, flags);
     }
 
     @Override
