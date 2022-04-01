5743a32dedc6a254805e076e89775d37b864c74a
Lee <lee@benf.org>
2022-04-01 04:52:40 +0100

    break

--- decompilation-test/test-data-expected-output/classes/CondJumpTest2c.10.expected.java
+++ decompilation-test/test-data-expected-output/classes/CondJumpTest2c.10.expected.java
@@ -6,6 +6,6 @@ package org.benf.cfr.tests;
 public class CondJumpTest2c {
     public boolean test(boolean a, boolean b) {
         boolean c;
-        return b && a == (c = b) || b && (c = a);
+        return b && a == (c = x) || b && (c = a);
     }
 }
